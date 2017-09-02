deef0cf88378b12c67dce0c2fc6fd4069122e5c2
VidyaJeyapaul <31559659+VidyaJeyapaul@users.noreply.github.com>
2017-09-02 11:31:14 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,21 @@
+package guvi;
+import java.util.*;
+/**
+ *
+ * @author vidya
+ */
+public class evenorodd {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0)
+        {
+            System.out.print("Even");
+        }
+        else
+        {
+            System.out.print("Odd");
+        }
+}
+}
