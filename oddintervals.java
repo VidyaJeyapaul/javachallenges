b15966db443740c9b4a5a47ef0b3a254843f48a3
VidyaJeyapaul <31559659+VidyaJeyapaul@users.noreply.github.com>
2017-09-02 22:45:32 +0530

    Add files via upload

---
+++ oddintervals.java
@@ -0,0 +1,23 @@
+package guvi2;
+import java.util.*;
+
+/**
+ *
+ * @author vidya
+ */
+public class oddbtwnintervals {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int i;
+        for(i=n;i<=m;i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.print(i+ " ");
+            }
+        }
+    }    
+}
