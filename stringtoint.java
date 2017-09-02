428580c1aa747841cb9c0ecb83f3ec02244d2b72
VidyaJeyapaul <31559659+VidyaJeyapaul@users.noreply.github.com>
2017-09-02 23:25:01 +0530

    Add files via upload

---
+++ stringtoint.java
@@ -0,0 +1,17 @@
+package guvi3;
+import java.util.*;
+import java.lang.*;
+
+/**
+ *
+ * @author vidya
+ */
+public class stringtoint {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String n=sc.next();
+        int r=Integer.parseInt(n);
+        System.out.print(r);
+    }
+}
