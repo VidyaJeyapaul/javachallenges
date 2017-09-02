ce21031f1db67d5186c234641decda879f806dbf
VidyaJeyapaul <31559659+VidyaJeyapaul@users.noreply.github.com>
2017-09-02 11:56:18 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,17 @@
+package guvi;
+import java.util.*;
+
+/**
+ *
+ * @author vidya
+ */
+public class helloworld {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("Hello World");
+        }
+    }
+}
